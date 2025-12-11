0ddf084eda2bca65e2b117df6015b22f3618f5b7
Onyiriuka Justin <richardjustin500@gmail.com>
2025-12-11 16:33:45 +0100

    [fix] initials derival

--- src/lib/get-name-initials.ts
+++ src/lib/get-name-initials.ts
@@ -1,7 +1,10 @@
 export function getInitials(fullname: string) {
-  return fullname
-    ?.split(" ")
-    .map((n) => n[0].toUpperCase())
-    .slice(1)
-    .join("");
+  if (!fullname) return "";
+
+  const names = fullname.trim().split(/\s+/);
+
+  const firstInitial = names[0].charAt(0);
+  const lastInitial = names.length > 1 ? names[names.length - 1].charAt(0) : "";
+
+  return (firstInitial + lastInitial).toUpperCase();
 }
